2b16bce273e8f74614952ace771f1c0de0f736ce
lincw <chaolics@outlook.com>
2021-09-04 17:32:37 +0800

    change the verbose name AND the class name

--- netdisk/models.py
+++ netdisk/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 
-# Create your models here.
-class file():
-    name = models.TextField("File name", max_length=1024)
-    describe = models.TextField("Description", max_length=10240)
-    hash = models.TextField("MD5 hash of the file", max_length=16)
-    location = models.CharField("Location of the file", blank=True)
-    tags = models.JSONField("Tags of the file, which is an Array")
-    size = models.CharField("Size of the file, by KiB")
-    time = models.DateTimeField("Time file uploaded")
-    storage_provider = models.CharField("Provider of the storage: OSS/QiNiu/etc.")
-    storage_uri = models.CharField("URI of the file in given provider")
-    is_deleted = models.BooleanField("While file been deleted, the field is False")
-    is_folder = models.BooleanField("Whether this is a folder")
-    download_time_count = models.IntegerField("Download time count")
+# Class File
+class SingleFile(models.Model):
+    name = models.TextField(verbose_name="File name", max_length=1024)
+    describe = models.TextField(verbose_name="Description", max_length=10240)
+    hash = models.TextField(verbose_name="MD5 hash of the file", max_length=16)
+    location = models.CharField(verbose_name="Location of the file", blank=True)
+    tags = models.JSONField(verbose_name="Tags of the file, which is an Array")
+    size = models.CharField(verbose_name="Size of the file, by KiB")
+    time = models.DateTimeField(verbose_name="Time file uploaded")
+    storage_provider = models.CharField(verbose_name="Provider of the storage: OSS/QiNiu/etc.")
+    storage_uri = models.CharField(verbose_name="URI of the file in given provider")
+    is_deleted = models.BooleanField(verbose_name="While file been deleted, the field is False")
+    is_folder = models.BooleanField(verbose_name="Whether this is a folder")
+    download_time_count = models.IntegerField()
 
